941afab4ea0c6ca7473df14b66624ebb1ec2da36
V.Surendhar <vsurendharm@gmail.com>
2026-03-24 08:44:28 +0530

    feat: update health check endpoint to return JSON response

--- src/main/kotlin/com/voiddeveloper/tictactoe/InitController.kt
+++ src/main/kotlin/com/voiddeveloper/tictactoe/InitController.kt
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping
 class InitController {
 
     @RequestMapping("/")
-    fun isAlive() : ResponseEntity<String> {
-        return ResponseEntity.ok("I am alive")
+    fun isAlive(): ResponseEntity<String> {
+        return ResponseEntity.ok("""{"status": "alive"}""")
     }
+
 }
